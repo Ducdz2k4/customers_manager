1fc508d2bc6b6d9634a19b0c300be851d3426e48
Ducdz2k4 <161570072+Ducdz2k4@users.noreply.github.com>
2024-04-05 20:13:42 +0700

    init project

---
+++ src/main/java/com/mycompany/customers_manager/Customers_manager.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.customers_manager;
+
+/**
+ *
+ * @author nduc6
+ */
+public class Customers_manager {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
